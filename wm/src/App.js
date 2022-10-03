af5430909dfe5114a7fa9e65ffd5e5f6ce319c52
Erkhembayar-Atarbold <erkhembayar.atarbold.dev@gmail.com>
2022-10-03 07:41:59 -0700

    New component Header and Main built

--- wm/src/App.js
+++ wm/src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+
+      <Main />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <li>
+          <span>Company</span>
+        </li>
+      </nav>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <div>
+      {" "}
+      <img></img>{" "}
+    </div>
+  );
 }
 
 export default App;
